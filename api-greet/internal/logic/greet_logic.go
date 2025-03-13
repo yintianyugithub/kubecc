d58b0e3117d0541cd920085216f127368ccb2945
itianyu <itianyu@home.com>
2025-03-13 23:46:20 +0800

    api: greet.api rpc: add .gitignore makefile

--- api-greet/internal/logic/greet_logic.go
+++ api-greet/internal/logic/greet_logic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"greet/service/add/pb/add"
 
 	"greet/api-greet/internal/svc"
 	"greet/api-greet/internal/types"
@@ -25,6 +26,6 @@ func NewGreetLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GreetLogic 
 
 func (l *GreetLogic) Greet(req *types.Request) (resp *types.Response, err error) {
 	// todo: add your logic here and delete this line
-
+	_, err = l.svcCtx.RpcAdd.Add(l.ctx, &add.AddReq{})
 	return
 }
--- api-greet/internal/svc/service_context.go
+++ api-greet/internal/svc/service_context.go
@@ -2,10 +2,12 @@ package svc
 
 import (
 	"greet/api-greet/internal/config"
+	"greet/service/add/adder"
 )
 
 type ServiceContext struct {
 	Config config.Config
+	RpcAdd adder.Adder
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
